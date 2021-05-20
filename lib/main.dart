a781a029baaec45263326249d00388175715541e
Ritik <ritikkumar2742@gmail.com>
2021-05-20 18:25:20 +0530

    Project completed

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hostelapp/additional/SplashScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hostel Space",
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.pink[300],
+        accentColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
